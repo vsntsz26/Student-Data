acf6f1030d6657d68e5b06e35f67139e2cf1c999
beteg <viniciusa2609@gmail.com>
2022-12-27 16:59:21 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace oi
+{
+    partial class EYLN
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.EYFN = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.ADE = new System.Windows.Forms.Label();
+            this.txtr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(538, 253);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(123, 61);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Send Data";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(33, 103);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(241, 26);
+            this.txt1.TabIndex = 1;
+            // 
+            // EYFN
+            // 
+            this.EYFN.AutoSize = true;
+            this.EYFN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EYFN.Location = new System.Drawing.Point(29, 65);
+            this.EYFN.Name = "EYFN";
+            this.EYFN.Size = new System.Drawing.Size(160, 20);
+            this.EYFN.TabIndex = 2;
+            this.EYFN.Text = "Enter your first name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter your last name:";
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(33, 185);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(241, 26);
+            this.txt2.TabIndex = 4;
+            // 
+            // ADE
+            // 
+            this.ADE.AutoSize = true;
+            this.ADE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ADE.Location = new System.Drawing.Point(476, 65);
+            this.ADE.Name = "ADE";
+            this.ADE.Size = new System.Drawing.Size(125, 20);
+            this.ADE.TabIndex = 5;
+            this.ADE.Text = "All data entered:";
+            // 
+            // txtr
+            // 
+            this.txtr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtr.Location = new System.Drawing.Point(480, 88);
+            this.txtr.Multiline = true;
+            this.txtr.Name = "txtr";
+            this.txtr.Size = new System.Drawing.Size(241, 123);
+            this.txtr.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter your age:";
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(33, 269);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(241, 26);
+            this.txt4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter your course:";
+            // 
+            // txt5
+            // 
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.Location = new System.Drawing.Point(33, 357);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(241, 26);
+            this.txt5.TabIndex = 10;
+            // 
+            // EYLN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtr);
+            this.Controls.Add(this.ADE);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EYFN);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.Button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "EYLN";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label EYFN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label ADE;
+        private System.Windows.Forms.TextBox txtr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oi
+{
+    public partial class EYLN : Form
+    {
+        public EYLN()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            string firstName = txt1.Text;
+            string lastName = txt2.Text;
+            int age = Convert.ToInt32 (txt4.Text);
+            string course = txt5.Text;
+
+            txtr.Text = "Nome: " + firstName + " " + lastName
+                + "\r\nAge: " + age + "\r\nCourse: " + course;
+        }
+    }
+
+}
